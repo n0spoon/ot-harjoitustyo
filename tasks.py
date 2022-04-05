9162e1f3d02b1f75942e85f57af622b518debfd8
n0spoon <xtrifo@gmail.com>
2022-04-05 23:05:57 +0300

    fixed task.py coverage commands

--- tasks.py
+++ tasks.py
@@ -15,7 +15,7 @@ def lorem_ipsum(ctx):
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
