6277b5d66696ab9aff63e3ab37a02679d823302d
n0spoon <xtrifo@gmail.com>
2022-05-02 22:28:19 +0300

    Docstring documentation for calculation_service.py and calculation_repository.py

--- src/repositories/calculation_repository.py
+++ src/repositories/calculation_repository.py
@@ -1,12 +1,39 @@
 class CalculationRepository:
+    """Luokka, joka säilöö laskutoimitukset.
+    """
+
     def __init__(self):
+        """Luokan konstruktori, joka luo uuden taulukon.
+
+        Args:
+            _calculations: Taulukko
+        """
+
         self._calculations = []
 
     def add_calculation(self, content):
+        """Lisää laskutoimituksen taulukkoon.
+
+        Args:
+            content (int or float): Laskutoimitus.
+        """
+
         self._calculations.append(content)
 
     def print_calculations(self):
+        """Tulostaa listan laskutoimituksista.
+
+        Returns:
+            Lista laskutoimituksista.
+        """
+
         return print(self._calculations)
 
     def count_calculations(self):
+        """Palauttaa taulukon koon.
+
+        Returns:
+            int: Taulukon koko.
+        """
+
         return len(self._calculations)
--- src/services/calculation_service.py
+++ src/services/calculation_service.py
@@ -2,10 +2,33 @@ from repositories.calculation_repository import CalculationRepository
 
 
 class CalculationService:
+    """Luokka, joka muuntaa käyttäjän syötteen numeroksi ja laskee käyttäjän määrittämän laskutoimituksen.
+
+    Attributes:
+        _calcdata: CalculationRepository instanssi, johon talletetaan laskutoimituksien tulokset.
+    """
+
     def __init__(self):
+        """Luokan konstruktori.  Luo uuden CalculationRepository-olion.
+
+        Args:
+            _calcdata: CalculationRepository instanssi, johon talletetaan laskutoimitusten tulokset.
+        """
+
         self._calcdata = CalculationRepository()
 
     def string_to_number(self, var_s):
+        """Muuntaa merkkijonon numeroksi mikäli se on joko liuku- tai kokonaisluku.
+
+        Args:
+            var_s (str): Merkkijono, joka kuvaa numeroa.
+
+        Returns:
+            Tulostaa virheviestin, mikäli merkkijono ei ole kokonaisluku eikä liukuluku(jossa on käytetty pistettä erottamaan desimaaliosa).
+            int: Jos käyttäjän syöte on kokonaisluku.
+            float: Jos käyttäjän syöte on liukuluku.
+        """
+
         try:
             return int(str(var_s), 0)
         except ValueError:
@@ -15,6 +38,17 @@ class CalculationService:
                 return print(f"Error: {var_s} is not a number")
 
     def sum_service(self, var_a, var_b):
+        """Laskee kahden luvun summan.
+
+        Args:
+            var_a (int or float): Käyttäjän syöttämä kokonais- tai liukuluku.
+            var_b (int or float): Käyttäjän syöttämä kokonais- tai liukuluku.
+
+        Returns:
+            None olio, jos vähintään toinen muuttujista var_a tai var_b ei ole numero.
+            Muuten tulostaa summalaskun.
+        """
+
         try:
             var_x = self.string_to_number(var_a)
             var_y = self.string_to_number(var_b)
@@ -26,6 +60,16 @@ class CalculationService:
             return ""
 
     def sub_service(self, var_a, var_b):
+        """Laskee kahden luvun erotuksen.
+
+        Args:
+            var_a (int or float): Käyttäjän syöttämä kokonais- tai liukuluku.
+            var_b (int or float): Käyttäjän syöttämä kokonais- tai liukuluku.
+
+        Returns:
+            None olio, jos vähintään toinen muuttujista var_a tai var_b ei ole numero.
+            Muuten tulostaa erotuslaskun.
+        """
         try:
             var_x = self.string_to_number(var_a)
             var_y = self.string_to_number(var_b)
@@ -37,6 +81,17 @@ class CalculationService:
             return ""
 
     def mul_service(self, var_a, var_b):
+        """Laskee kahden luvun kertolaskun.
+
+        Args:
+            var_a (int or float): Käyttäjän syöttämä kokonais- tai liukuluku.
+            var_b (int or float): Käyttäjän syöttämä kokonais- tai liukuluku.
+
+        Returns:
+            None olio, jos vähintään toinen muuttujista var_a tai var_b ei ole numero.
+            Muuten tulostaa kertoslaskun.
+        """
+
         try:
             var_x = self.string_to_number(var_a)
             var_y = self.string_to_number(var_b)
@@ -48,6 +103,18 @@ class CalculationService:
             return ""
 
     def div_service(self, var_a, var_b):
+        """Laskee kahden luvun jakolaskun.
+
+        Args:
+            var_a (int or float): Käyttäjän syöttämä kokonais- tai liukuluku.
+            var_b (int or float): Käyttäjän syöttämä kokonais- tai liukuluku.
+
+        Returns:
+            None olio, jos vähintään toinen muuttujista var_a tai var_b ei ole numero.
+            Tulostaa virheviestin, jos jakajan arvo on nolla.
+            Muuten tulostaa jakolaskun.
+        """
+
         try:
             var_x = self.string_to_number(var_a)
             var_y = self.string_to_number(var_b)
@@ -61,6 +128,17 @@ class CalculationService:
             return ""
 
     def sqrt_service(self, var_a):
+        """Laskee reaaliluvun neliöjuuren.
+
+        Args:
+            var_a (int or float): Käyttäjän syöttämä kokonais- tai liukuluku.
+
+        Returns:
+            None olio, jos muuttuja var_a ei ole numero.
+            Tulostaa virheviestin, jos luku ei ole positiivinen.
+            Muuten tulostaa luvun neliöjuuren.
+        """
+
         try:
             var_x = self.string_to_number(var_a)
             if var_x < 0:
@@ -73,6 +151,18 @@ class CalculationService:
             return ""
 
     def exp_service(self, var_a, var_b):
+        """Laskee exponenttilaskun, jossa var_a on kantaluku ja var_b eksponentti.
+
+        Args:
+            var_a (int or float): Käyttäjän syöttämä kokonais- tai liukuluku, kantaluku.
+            var_b (int or float): Käyttäjän syöttämä kokonais- tai liukuluku, eksponentti.
+
+        Returns:
+            None olio, jos vähintään toinen muuttujista var_a tai var_b ei ole numero.
+            Tulostaa virheviestin, jos kantaluku on nolla ja eksponentti negatiivinen.
+            Muuten tulostaa eksponenttilaskun.
+        """
+
         try:
             var_x = self.string_to_number(var_a)
             var_y = self.string_to_number(var_b)
@@ -81,12 +171,24 @@ class CalculationService:
                 return "Error: Division by Zero isn't allowed\n"
             result = var_x**var_y
             result = self.clean_result(result)
+            if var_y == 0.5:
+                result = f"±{self.clean_result(result)}"
             self.add_result(result)
             return f"{var_x}^{var_y} = {result}\n"
         except TypeError:
             return ""
 
     def clean_result(self, result):
+        """Siistii annetun numeron.
+
+        Args:
+            result (int or float): Laskutoimituksen tulos.
+
+        Returns:
+            int: Jos numero on liukuluku, jonka desimaaliosa on nolla.
+            float: Jos numero on liukuluku, jonka desimaaliosa ei ole nolla.
+        """
+
         pieces = str(result).split(".")
         if len(pieces[-1]) == 1:
             last = int(pieces[-1])
@@ -95,10 +197,27 @@ class CalculationService:
         return result
 
     def count(self):
+        """Laskee laskutoimitusten määrän.
+
+        Returns:
+            int: Laskutoimitusten määrä.
+        """
         return self._calcdata.count_calculations()
 
     def return_calculations(self):
+        """Tulostaa lasketut laskutoimitukset.
+
+        Returns:
+            Lista laskutoimituksista.
+        """
+
         return self._calcdata.print_calculations()
 
     def add_result(self, result):
+        """Tallentaa laskutoimituksen _calcdata-olioon.
+
+        Args:
+            result (int or float): Laskutoimituksen tulos.
+        """
+
         self._calcdata.add_calculation(result)
